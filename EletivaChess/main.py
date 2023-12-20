493445ebd14ce116a826abf071593c446b64e3a1
Gustavo Borges Hertz <127430409+gustavohertz@users.noreply.github.com>
2023-12-20 08:58:14 -0300

    Update main.py

--- EletivaChess/main.py
+++ EletivaChess/main.py
@@ -1,20 +1,27 @@
+import chessdotcom
 import requests
-from chessdotcom import get_leaderboards
+from chessdotcom import get_leaderboards, get_player_stats
 import pprint
 
 printer = pprint.PrettyPrinter()
 
+# Configurar o User-Agent
+headers = {'User-Agent': 'My Python Application. Contact me at email@example.com'}
+chessdotcom.Client.request_config['headers'] = headers
+
 def print_leaderboards():
-    user_agent = 'My Python Application. Contact me at gustavo.hertz@al.infnet.edu.br'
-    
-    headers = {'User-Agent': 'chess.com analysis app / contact: gustavo.hertz@al.infnet.edu.br'}
     response = requests.get('https://api.chess.com/pub/leaderboards', headers=headers)
   
     data = response.json()
     categories = data.keys()
     for category in categories:
-            print('Category:',category)
-            for idx, entry in enumerate(data[category]):
-                print(f'Rank:{idx + 1}| UserName: {entry["username"]} |Rating: {entry["score"]}')
- 
+        print('Category:', category)
+        for idx, entry in enumerate(data[category]):
+            print(f'Rank:{idx + 1}| UserName: {entry["username"]} |Rating: {entry["score"]}')
+
+def get_player_rating(username):
+    data = get_player_stats(username).json
+    printer.pprint(data)
+
 print_leaderboards()
+get_player_rating("GustavoHertz")
